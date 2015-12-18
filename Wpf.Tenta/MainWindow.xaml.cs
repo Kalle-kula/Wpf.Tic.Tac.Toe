73f11aad4e3748240fddf8c573aa5cbbc356c039
Kalle <Kalle.hallert@gmail.com>
2015-12-18 12:00:15 +0100

    Added comments to code

--- Wpf.Tenta/MainWindow.xaml.cs
+++ Wpf.Tenta/MainWindow.xaml.cs
@@ -74,7 +74,7 @@ namespace Wpf.Tenta
             Button(0, 2, board);
         }
 
-        //Kollar om det är X eller O som sätts ut och sätter vem som ska lägga härnaäst:
+        //Kollar om det är X eller O som sätts ut och sätter vem som ska lägga härnäst:
         void Button(int p1, int p2, char[,] board)
         {
             if (InputToGrid(p1, p2, board, circleTurn))
@@ -102,20 +102,21 @@ namespace Wpf.Tenta
 
         void SetButtons()
         {
-            //Top
+            //Topp
             Btn1.Content = board[2, 0];
             Btn2.Content = board[2, 1];
             Btn3.Content = board[2, 2];
-            //Middle
+            //Mitten
             Btn4.Content = board[1, 0];
             Btn5.Content = board[1, 1];
             Btn6.Content = board[1, 2];
-            //Bottom
+            //Botten
             Btn7.Content = board[0, 0];
             Btn8.Content = board[0, 1];
             Btn9.Content = board[0, 2];
         }
 
+        //Skriver ut vems tur det är
         void SetTurnMsg(bool noughtTurn)
         {
             WhosTurn.Content = "";
@@ -135,7 +136,7 @@ namespace Wpf.Tenta
         }
 
 
-        //Kollar vinnare (eller om det blev lika)
+        //Kollar vinnare eller om det blev lika (resultatet)
         bool StateCheck()
         {
             switch (CheckBoard(board))
@@ -180,55 +181,44 @@ namespace Wpf.Tenta
             return ' ';
         }
 
-        /// <summary>
-        /// Returns the state of the board in terms of Noughts and Crosses Rules
-        /// O or X == Naught\Cross win
-        /// F == Full board
-        /// ' ' == Nothing to stop play
-        /// </summary>
+        //Kontrollerar hur spelplanen ser ut och utser vinnare enligt 3-i-rad reglerna
+        
         static char CheckBoard(char[,] board)
         {
             if ((OX(board[1, 1]) == "O"[0] || OX(board[1, 1]) == "X"[0]) && (board[2, 2] == board[1, 1] && board[1, 1] == board[0, 0]))
             {
-                //Diagonal
                 return OX(board[1, 1]);
             }
             else if ((OX(board[1, 1]) == "O"[0] || OX(board[1, 1]) == "X"[0]) && (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2]))
             {
-                //Diagonal
                 return OX(board[1, 1]);
             }
             else if ((OX(board[2, 1]) == "O"[0] || OX(board[2, 1]) == "X"[0]) && (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2]))
             {
-                //Top row
                 return OX(board[2, 1]);
             }
             else if ((OX(board[1, 1]) == "O"[0] || OX(board[1, 1]) == "X"[0]) && (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2]))
             {
-                //Middle row
                 return OX(board[1, 1]);
             }
             else if ((OX(board[0, 1]) == "O"[0] || OX(board[0, 1]) == "X"[0]) && (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2]))
             {
-                //Bottom row
                 return OX(board[0, 1]);
             }
             else if ((OX(board[1, 0]) == "O"[0] || OX(board[1, 0]) == "X"[0]) && (board[2, 0] == board[1, 0] && board[1, 0] == board[0, 0]))
             {
-                //left collumn
                 return OX(board[1, 0]);
             }
             else if ((OX(board[1, 1]) == "O"[0] || OX(board[1, 1]) == "X"[0]) && (board[2, 1] == board[1, 1] && board[1, 1] == board[0, 1]))
             {
-                //middle collumn
                 return OX(board[1, 1]);
             }
             else if ((OX(board[1, 2]) == "O"[0] || OX(board[1, 2]) == "X"[0]) && (board[2, 2] == board[1, 2] && board[1, 2] == board[0, 2]))
             {
-                //right collumn
                 return OX(board[1, 2]);
             }
 
+            //Räknar antal satta spelknappar och returnerar F (fullt) om det är nio eller mer
             int filledTiles = 0;
 
             for (int i = 0; i < board.GetLength(0); i++)
@@ -241,7 +231,7 @@ namespace Wpf.Tenta
                     }
                 }
             }
-
+            
             if (filledTiles >= 9)
             {
                 return 'F';
